e3963168af16d8e0c471992201c044baaf2c177f
vivek joshi <reachvivekjoshi@gmail.com>
2020-07-23 16:16:30 +0530

    contains routes to home, registeration and login pages

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '9696ecf37b34150ed2fed7a88beb596b'
 
 
 @app.route("/")
+@app.route("/home")
 def index():
     return render_template('index.html')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('index'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
